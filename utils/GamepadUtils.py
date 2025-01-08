7779190cf48d27f9dc3ee575bb75bcc85f3fec3b
Ben Miller <52042531+bdmiller42@users.noreply.github.com>
2025-01-08 17:51:48 -0500

    Updated RobotContainer for the defualt commands it eliminated the need for the "utils" folder

--- utils/GamepadUtils.py
+++
@@ -1,8 +0,0 @@
-import wpimath
-
-class GamepadUtils:
-    @staticmethod
-    def squareInput(value, deadband):
-        """Squares the input value with deadband handling."""
-        value_with_deadband = wpimath.applyDeadband(value, deadband)
-        return (value_with_deadband ** 2) * (1 if value >= 0 else -1)
